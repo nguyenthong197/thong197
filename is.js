0a6955d226dde4b47dbb9b9a3f689b4e5b469745
Nguyễn Thống <90368222+nguyenthong197@users.noreply.github.com>
2021-09-16 09:46:03 +0700

    Add files via upload

---
+++ is.js
@@ -0,0 +1,7 @@
+function hienthi() {
+			var node = document.getElementById("anhthong");
+			node.innerHTML = "<img src='imega/60.jpg'>"
+		}
+		window.alert("Bạn có muốn vào trang này không?")
+		window.confirm("Trang này rất nguy hiểm?")
+		window.prompt("Bạn vào đây với mục đích gì?")
